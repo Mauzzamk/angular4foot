414e0b86c9517ea068d834be73f5e1b0107be5ac
Mauzzam Khan <khanmauzzam02.com>
2018-06-24 19:07:34 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import{LayoutModule} from './layout/layout.module';
+import {LoginModule} from './login/login.module'
+import { UserService } from './user.service';
+import { RouterModule } from '@angular/router';
+import { LoginFormComponent } from './login/login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [RouterModule.forRoot([
+    {path: '', component : LoginFormComponent},
+    {path: 'layout', loadChildren: 'app/layout/layout.module#LayoutModule'},
+    
+]),
+    BrowserModule,LayoutModule,LoginModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
