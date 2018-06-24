8345de86490ce4387897e38b11d5743a3b688c19
Mauzzam Khan <khanmauzzam02.com>
2018-06-24 23:54:32 +0530

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import{LayoutModule} from './layout/layout.module';
-import {LoginModule} from './login/login.module'
+
 import { UserService } from './user.service';
-import { RouterModule } from '@angular/router';
-import { LoginFormComponent } from './login/login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { Router, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,FooterComponent,HeaderComponent,LoginFormComponent, DashboardComponent
   ],
-  imports: [RouterModule.forRoot([
-    {path: '', component : LoginFormComponent},
-    {path: 'layout', loadChildren: 'app/layout/layout.module#LayoutModule'},
-    
-]),
-    BrowserModule,LayoutModule,LoginModule
+  imports: [
+
+RouterModule.forRoot([{
+path : 'dashboard' , component : DashboardComponent
+
+}]),
+
+    BrowserModule,
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../user.service';
+
 import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login-form',
